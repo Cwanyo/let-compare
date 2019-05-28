65005283a34fbe15e9f3e07356e60f0bcadbed36
Cwanyo <manwan_444@hotmail.com>
2019-05-29 03:26:55 +0700

    es6 import

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+declare module "let-compare" {
+  /**
+   * Deep compare both values.
+   *
+   * @param {*} a
+   * @param {*} b
+   * @returns {boolean} Returns `true` if both values are same, else `false`.
+   */
+  export function letCompare(a: any, b: any): boolean;
+}
