977464e99ee466bab3fa7759c5e8255fbc7192e1
Cwanyo <manwan_444@hotmail.com>
2019-05-29 03:21:26 +0700

    add comment

--- src/index.js
+++ src/index.js
@@ -70,6 +70,13 @@ function isEqual(a, b) {
   }
 }
 
+/**
+ * Deep compare both values.
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns {boolean} Returns `true` if both values are same, else `false`.
+ */
 function letCompare(a, b) {
   try {
     return isEqual(a, b);
